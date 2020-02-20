33a7b0951d77bd38e7b06d87827056d9d110dc2c
James Grunewald <james.grunewald.10@ucl.ac.uk>
2020-02-20 15:35:49 +0000

    backend complete

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,33 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = find_cocktail
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = find_cocktail
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail.id)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail.id)
+  end
+
+  private
+
+  def find_cocktail
+    Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
